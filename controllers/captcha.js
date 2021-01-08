ae8f8e39beaf65301f77b5bd065071caf2d777cd
Yickson Ramírez <yickson@duemint.com>
2021-01-07 21:49:26 -0300

    Insercion de Logs para ver monitoreo

--- controllers/captcha.js
+++ controllers/captcha.js
@@ -3,7 +3,8 @@ const captcha = require('../models/captcha');
 const controller = {
     getCaptcha: async (req, res) => {
         const { base64 } = req.body;
-
+        console.log('Ejecutando captcha...');
+        console.log('Base64: ', base64);
         const captchaResolve = await captcha(base64);
 
         return res.status(200).send({
